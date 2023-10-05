bf36eef135b66fc56b92d16cbc7652da14f4ec2a
yagouam <yago.bernabe@uam.es>
2023-10-05 14:02:29 +0200

    Add created and modified fields into admin

--- uamx_tos_acceptance/admin.py
+++ uamx_tos_acceptance/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from .models import TermsOfService
 
 class TermsOfServiceAdmin(admin.ModelAdmin):
+    readonly_fields = ('created', 'modified')
     list_display = ('user', 'accepted')
 
 admin.site.register(TermsOfService, TermsOfServiceAdmin)
