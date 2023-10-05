f01f37944261496c0fbc8e96962c5f2fde1ab01b
yagouam <56433614+yagouam@users.noreply.github.com>
2023-10-05 17:26:56 +0200

    Enable sort by creation / modification date

--- uamx_tos_acceptance/admin.py
+++ uamx_tos_acceptance/admin.py
@@ -3,6 +3,6 @@ from .models import TermsOfService
 
 class TermsOfServiceAdmin(admin.ModelAdmin):
     readonly_fields = ('created', 'modified')
-    list_display = ('user', 'accepted')
+    list_display = ('user', 'accepted', 'created', 'modified')
 
 admin.site.register(TermsOfService, TermsOfServiceAdmin)
