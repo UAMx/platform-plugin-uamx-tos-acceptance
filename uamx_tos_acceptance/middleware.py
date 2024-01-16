3112eb997c32e2e1865be81dc6938a584ec54703
yagouam <56433614+yagouam@users.noreply.github.com>
2024-01-16 12:36:20 +0100

    Listen to API login_session calls and redirect if needed #1

--- uamx_tos_acceptance/middleware.py
+++ uamx_tos_acceptance/middleware.py
@@ -1,4 +1,10 @@
+import json
+
+from django.conf import settings
+from django.http import JsonResponse
 from django.shortcuts import redirect
+
+from openedx.core.djangoapps.site_configuration import helpers as configuration_helpers
 
 from .models import TermsOfService
 
@@ -22,16 +28,27 @@ class UAMxTermsOfServiceMiddleware:
 
         response = self.get_response(request)
 
-        # Check if user is uthenticated
+        # Check if user is authenticated
         if request.user.is_authenticated:
 
             # Check the state of TOS acceptance
             accepted = TermsOfService.objects.filter(user=request.user, accepted=True).exists()
-            
-            should_block_url = any(request.path.startswith(x) for x in ('/learning', '/dashboard', '/courses', '/u/{}'.format(request.user.username), '/account/settings', '/course_mode'))
 
             # Redirect ONLY if user has not accepted the TOS
-            if not accepted and should_block_url:
-                return redirect('/uamx_tos_acceptance')
-            
+            if not accepted:
+
+                # catch api login_session response and modify redirect_url
+                # to redirect user to TOS
+                if request.path == '/api/user/v2/account/login_session/':
+                    lms_root_url = configuration_helpers.get_value('LMS_ROOT_URL', settings.LMS_ROOT_URL)
+                    parsed = json.loads(response.content.decode('utf-8'))
+                    parsed['redirect_url']='{}/uamx_tos_acceptance'.format(lms_root_url)
+                    return JsonResponse(parsed, safe=False)
+
+                # While in TOS the menu is still visible, this prevents
+                # user from navigate to new page
+                should_block_url = any(request.path.startswith(x) for x in ('/learning', '/dashboard', '/courses', '/u/{}'.format(request.user.username), '/account/settings', '/course_mode'))
+                if should_block_url:
+                    return redirect('/uamx_tos_acceptance')
+
         return response
