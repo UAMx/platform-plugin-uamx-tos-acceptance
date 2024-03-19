c97b252c3057f75ac4ca04be55aab0dc7204ce31
yagouam <yago.bernabe@uam.es>
2024-03-19 12:07:01 +0100

    Add user search to admin

--- uamx_tos_acceptance/admin.py
+++ uamx_tos_acceptance/admin.py
@@ -3,6 +3,7 @@ from .models import TermsOfService
 
 class TermsOfServiceAdmin(admin.ModelAdmin):
     readonly_fields = ('created', 'modified')
+    search_fields = ['user__email', 'user__username']
     list_display = ('user', 'accepted_tos', 'accepted_privacy', 'created', 'modified')
 
 admin.site.register(TermsOfService, TermsOfServiceAdmin)
