4d3c40b1021bda3fdec189411e76c6afae35cd92
yagouam <yago.bernabe@uam.es>
2025-03-18 10:56:11 +0100

    Update version number to 0.1.3

--- platform_plugin_uamx_tos_acceptance/__init__.py
+++ platform_plugin_uamx_tos_acceptance/__init__.py
@@ -4,6 +4,6 @@ Enable users to accept the terms of service
 import os
 from pathlib import Path
 
-__version__ = '0.1.2'
+__version__ = '0.1.3'
 
 ROOT_DIRECTORY = Path(os.path.dirname(os.path.abspath(__file__)))
