f77952bc267269a1f9cefe626e38b8dda92f0c15
yagouam <yago.bernabe@uam.es>
2025-03-18 15:28:55 +0100

    Try to import app dynamically

--- platform_plugin_uamx_tos_acceptance/settings/common.py
+++ platform_plugin_uamx_tos_acceptance/settings/common.py
@@ -1,16 +1,20 @@
 # Plugin settings.py overrides
 
-from os.path import abspath, dirname, join
-
-from platform_plugin_uamx_tos_acceptance import ROOT_DIRECTORY
-
-
-INSTALLED_APPS = [
-    "platform_plugin_uamx_tos_acceptance",
-]
+from collections import OrderedDict
+from django.apps import apps
 
 # lint-amnesty, pylint: disable=missing-function-docstring, missing-module-docstring
 def plugin_settings(settings):
+
+    settings.INSTALLED_APPS += ("platform_plugin_uamx_tos_acceptance", )
+    # To load the new app let's reset app_configs, the dictionary
+    # with the configuration of loaded apps
+    apps.app_configs = OrderedDict()
+    # set ready to false so that populate will work 
+    apps.ready = False
+    # re-initialize them all; is there a way to add just one without reloading them all?
+    apps.populate(settings.INSTALLED_APPS)
+
     """
     Set of plugin settings used by the Open Edx platform.
     More info: https://github.com/edx/edx-platform/blob/master/openedx/core/djangoapps/plugins/README.rst
