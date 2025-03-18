2932d3ea5eb48566fcfa840d9ea609a16e9a64a0
yagouam <yago.bernabe@uam.es>
2025-03-18 15:11:38 +0100

    Add INSTALLED_APPS to plugin settings to check installation

--- platform_plugin_uamx_tos_acceptance/settings/production.py
+++ platform_plugin_uamx_tos_acceptance/settings/production.py
@@ -1,7 +1,20 @@
 # Plugin settings.py overrides
 
+from os.path import abspath, dirname, join
+
+from platform_plugin_uamx_tos_acceptance import ROOT_DIRECTORY
+
+
+INSTALLED_APPS = [
+    "platform_plugin_uamx_tos_acceptance",
+]
+
 # lint-amnesty, pylint: disable=missing-function-docstring, missing-module-docstring
 def plugin_settings(settings):
+    """
+    Set of plugin settings used by the Open Edx platform.
+    More info: https://github.com/edx/edx-platform/blob/master/openedx/core/djangoapps/plugins/README.rst
+    """
 
     # Add UAMxTermsOfServiceMiddleware middleware to the list of Django middlewares
     settings.MIDDLEWARE += ['platform_plugin_uamx_tos_acceptance.middleware.UAMxTermsOfServiceMiddleware']
