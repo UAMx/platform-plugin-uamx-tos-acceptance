bac3ea0e7c3d94eb3a305ffa3408fec74c1e48ce
yagouam <yago.bernabe@uam.es>
2025-07-22 10:05:26 +0200

    Remove old folders

--- uamx_tos_acceptance/settings/production.py
+++
@@ -1,7 +0,0 @@
-# Plugin settings.py overrides
-
-# lint-amnesty, pylint: disable=missing-function-docstring, missing-module-docstring
-def plugin_settings(settings):
-
-    # Add UAMxTermsOfServiceMiddleware middleware to the list of Django middlewares
-    settings.MIDDLEWARE += ['uamx_tos_acceptance.middleware.UAMxTermsOfServiceMiddleware']
